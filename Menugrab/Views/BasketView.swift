3c2dac669cbfbfb95d5bd8170ea17081edf12d51
Guillermo Alfonso Varela Chouciño <guillevch@gmail.com>
2020-12-31 04:58:54 +0100

    feat: basket view

--- Menugrab/Views/BasketView.swift
+++ Menugrab/Views/BasketView.swift
@@ -12,9 +12,9 @@ struct BasketView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             ZStack {
-                HStack {
+                HStack(alignment: .center) {
                     Button(action: { presentationMode.wrappedValue.dismiss() }) {
                         Image(systemName: "xmark")
                             .font(.system(size: 20))
@@ -26,72 +26,88 @@ struct BasketView: View {
                     .myFont(size: 17, weight: .medium)
             }
             .padding()
-            .frame(height: 54)
-            HStack {
-                Text("San Tung")
-                    .myFont(size: 23, weight: .bold)
-                Image(systemName: "chevron.right")
-                    .font(.system(size: 17))
-                    .foregroundColor(.myPrimary)
-                
-            }
-            .padding(.horizontal, 5)
-            .background(
+            .frame(height: 48)
+            Divider()
+                .opacity(0.3)
+            ScrollView {
                 VStack {
-                    Spacer()
-                    Color.myPrimaryLight
-                        .frame(height: 4)
-                        .offset(x: 0, y: -8)
-                }
-            )
-            .padding(.vertical)
-            HStack(spacing: 10) {
-                Image("pickup-icon")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .scaledToFit()
-                    .frame(width: 25, height: 25)
-                Text("Pick up the order at the restaurant")
-                    .myFont(size: 15)
-                Spacer()
-            }.padding(.horizontal)
-            Divider().opacity(0.5).padding(.horizontal)
-            HStack(spacing: 10) {
-                Image(systemName: "map")
-                    .font(.system(size: 20))
-                VStack(alignment: .leading, spacing: 4) {
                     HStack {
                         Text("San Tung")
+                            .myFont(size: 23, weight: .bold)
+                        Image(systemName: "chevron.right")
+                            .font(.system(size: 17))
+                            .foregroundColor(.myPrimary)
+                        
+                    }
+                    .padding(.horizontal, 5)
+                    .background(
+                        VStack {
+                            Spacer()
+                            Color.myPrimaryLight
+                                .frame(height: 5)
+                                .offset(x: 0, y: -7)
+                        }
+                    )
+                    .padding(.vertical)
+                    HStack(spacing: 10) {
+                        Image("pickup-icon")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .scaledToFit()
+                            .frame(width: 25, height: 25)
+                        Text("Pick up the order at the restaurant")
                             .myFont(size: 15)
                         Spacer()
-                        Text("View map")
-                            .myFont(size: 13, weight: .medium, color: .gray)
-                    }
-                    Text("Wolf Crater, 897, Marsh")
-                        .myFont(size: 13, color: .gray)
-                    Text("3 km away")
-                        .myFont(size: 13, color: .gray)
+                    }.padding(.horizontal)
+                    Divider().opacity(0.5).padding(.horizontal)
+                    HStack(spacing: 10) {
+                        Image(systemName: "map")
+                            .font(.system(size: 20))
+                        VStack(alignment: .leading, spacing: 4) {
+                            HStack {
+                                Text("San Tung")
+                                    .myFont(size: 15)
+                                Spacer()
+                                Text("View map")
+                                    .myFont(size: 13, weight: .medium, color: .gray)
+                            }
+                            Text("Wolf Crater, 897, Marsh")
+                                .myFont(size: 13, color: .gray)
+                            Text("3 km away")
+                                .myFont(size: 13, color: .gray)
+                        }
+                    }.padding(.horizontal)
+                    Divider().opacity(0.5).padding(.horizontal)
+                    HStack(spacing: 10) {
+                        Image(systemName: "clock")
+                            .font(.system(size: 20))
+                        Text("You will notified with an estimate time to collect your order")
+                            .myFont(size: 15)
+                        Spacer()
+                    }.padding(.horizontal)
+                    Color.lightestGray.frame(height: 8).opacity(0.5)
+                    OrderItemsView()
+                        .padding()
+                    RoundedRectangle(cornerRadius: 16)
+                        .foregroundColor(Color.myPrimary)
+                        .frame(width: 325, height: 50, alignment: .center)
+                        .overlay(
+                            HStack {
+                                Text("Check out")
+                                    .myFont(size: 17, weight: .bold)
+                            }
+                        )
+                        .padding(.vertical)
+                    Spacer()
                 }
-            }.padding(.horizontal)
-            Divider().opacity(0.5).padding(.horizontal)
-            HStack(spacing: 10) {
-                Image(systemName: "clock")
-                    .font(.system(size: 20))
-                Text("You will notified with an estimate time to collect your order")
-                    .myFont(size: 15)
-                Spacer()
-            }.padding(.horizontal)
-            Color.lightestGray.frame(height: 8).opacity(0.5)
-            OrderItemsView()
-                .padding()
-            Spacer()
+            }
         }
     }
 }
 
 fileprivate struct OrderItemsView: View {
     var body: some View {
-        VStack(spacing: 10) {
+        VStack(spacing: 8) {
             HStack {
                 Text("Your order")
                     .myFont(size: 17, weight: .medium)
