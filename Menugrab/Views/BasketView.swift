18ce345c4066f6b4f2db41dc9455ed569ea16a0c
Guillermo Alfonso Varela Chouciño <guillevch@gmail.com>
2021-01-01 15:22:04 +0100

    feat: zigzag background view

--- Menugrab/Views/BasketView.swift
+++ Menugrab/Views/BasketView.swift
@@ -12,96 +12,103 @@ struct BasketView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        VStack(spacing: 0) {
-            ZStack {
-                HStack(alignment: .center) {
-                    Button(action: { presentationMode.wrappedValue.dismiss() }) {
-                        Image(systemName: "xmark")
-                            .font(.system(size: 20))
-                            .foregroundColor(.myBlack)
-                    }
-                    Spacer()
-                }
-                Text("Your basket")
-                    .myFont(size: 17, weight: .medium)
-            }
-            .padding()
-            .frame(height: 48)
-            Divider()
-                .opacity(0.3)
+        GeometryReader { geometry in
             ScrollView {
-                VStack {
-                    HStack {
-                        Text("San Tung")
-                            .myFont(size: 23, weight: .bold)
-                        Image(systemName: "chevron.right")
-                            .font(.system(size: 17))
-                            .foregroundColor(.myPrimary)
-                        
-                    }
-                    .padding(.horizontal, 5)
-                    .background(
-                        VStack {
+                VStack(spacing: 0) {
+                    ZStack {
+                        HStack(alignment: .center) {
+                            Button(action: { presentationMode.wrappedValue.dismiss() }) {
+                                Image(systemName: "xmark")
+                                    .font(.system(size: 20))
+                                    .foregroundColor(.myBlack)
+                            }
                             Spacer()
-                            Color.myPrimaryLight
-                                .frame(height: 5)
-                                .offset(x: 0, y: -7)
                         }
-                    )
-                    .padding(.vertical)
-                    HStack(spacing: 10) {
-                        Image("pickup-icon")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .scaledToFit()
-                            .frame(width: 25, height: 25)
-                        Text("Pick up the order at the restaurant")
-                            .myFont(size: 15)
-                        Spacer()
-                    }.padding(.horizontal)
-                    Divider().opacity(0.5).padding(.horizontal)
-                    HStack(spacing: 10) {
-                        Image(systemName: "map")
-                            .font(.system(size: 20))
-                        VStack(alignment: .leading, spacing: 4) {
+                        Text("Your basket")
+                            .myFont(size: 17, weight: .medium)
+                    }
+                    .padding()
+                    .frame(height: 48)
+                    Divider()
+                        .opacity(0.3)
+                    ScrollView {
+                        VStack {
                             HStack {
                                 Text("San Tung")
+                                    .myFont(size: 23, weight: .bold)
+                                Image(systemName: "chevron.right")
+                                    .font(.system(size: 17))
+                                    .foregroundColor(.myPrimary)
+                                
+                            }
+                            .padding(.horizontal, 5)
+                            .background(
+                                VStack {
+                                    Spacer()
+                                    Color.myPrimaryLight
+                                        .frame(height: 5)
+                                        .offset(x: 0, y: -7)
+                                }
+                            )
+                            .padding(.vertical)
+                            HStack(spacing: 10) {
+                                Image("pickup-icon")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .scaledToFit()
+                                    .frame(width: 25, height: 25)
+                                Text("Pick up the order at the restaurant")
                                     .myFont(size: 15)
                                 Spacer()
-                                Text("View map")
-                                    .myFont(size: 13, weight: .medium, color: .gray)
+                            }.padding(.horizontal)
+                            Divider().opacity(0.5).padding(.horizontal)
+                            HStack(spacing: 10) {
+                                Image(systemName: "map")
+                                    .font(.system(size: 20))
+                                VStack(alignment: .leading, spacing: 4) {
+                                    HStack {
+                                        Text("San Tung")
+                                            .myFont(size: 15)
+                                        Spacer()
+                                        Text("View map")
+                                            .myFont(size: 13, weight: .medium, color: .gray)
+                                    }
+                                    Text("Wolf Crater, 897, Marsh")
+                                        .myFont(size: 13, color: .gray)
+                                    Text("3 km away")
+                                        .myFont(size: 13, color: .gray)
+                                }
+                            }.padding(.horizontal)
+                            Divider().opacity(0.5).padding(.horizontal)
+                            HStack(spacing: 10) {
+                                Image(systemName: "clock")
+                                    .font(.system(size: 20))
+                                Text("You will notified with an estimate time to collect your order")
+                                    .myFont(size: 15)
+                                Spacer()
+                            }.padding(.horizontal)
+                            Color.lightestGray.frame(height: 8).opacity(0.5)
+                            OrderItemsView()
+                                .padding()
+                            ZStack {
+                                ZigZagBackgroundView(color: Color.lightestGray, numberOfTriangles: 20, triangleHeight: 10)
+                                
+                                RoundedRectangle(cornerRadius: 16)
+                                    .foregroundColor(Color.myPrimary)
+                                    .frame(width: 325, height: 50, alignment: .center)
+                                    .overlay(
+                                        HStack {
+                                            Text("Check out")
+                                                .myFont(size: 17, weight: .bold)
+                                        }
+                                    )
+                                    .padding(.vertical)
                             }
-                            Text("Wolf Crater, 897, Marsh")
-                                .myFont(size: 13, color: .gray)
-                            Text("3 km away")
-                                .myFont(size: 13, color: .gray)
                         }
-                    }.padding(.horizontal)
-                    Divider().opacity(0.5).padding(.horizontal)
-                    HStack(spacing: 10) {
-                        Image(systemName: "clock")
-                            .font(.system(size: 20))
-                        Text("You will notified with an estimate time to collect your order")
-                            .myFont(size: 15)
-                        Spacer()
-                    }.padding(.horizontal)
-                    Color.lightestGray.frame(height: 8).opacity(0.5)
-                    OrderItemsView()
-                        .padding()
-                    RoundedRectangle(cornerRadius: 16)
-                        .foregroundColor(Color.myPrimary)
-                        .frame(width: 325, height: 50, alignment: .center)
-                        .overlay(
-                            HStack {
-                                Text("Check out")
-                                    .myFont(size: 17, weight: .bold)
-                            }
-                        )
-                        .padding(.vertical)
-                    Spacer()
-                }
-            }
-        }
+                    }
+                }.background(Color.white)
+            }.background(Color.lightestGray)
+        }.edgesIgnoringSafeArea(.bottom)
     }
 }
 
---
+++ Menugrab/Views/Shared/ZigZagBackgroundView.swift
@@ -0,0 +1,40 @@
+//
+//  ZigZagSeparatorView.swift
+//  Menugrab
+//
+//  Created by Guillermo Alfonso Varela Chouciño on 01/01/2021.
+//
+
+import SwiftUI
+
+struct ZigZagBackgroundView: View {
+    let color: Color
+    let numberOfTriangles: Int
+    let triangleHeight: Int
+    
+    var body: some View {
+        ZStack {
+            color
+            GeometryReader { geometry in
+                let triangleWidth = Float(geometry.size.width) / Float(numberOfTriangles)
+                Path { path in
+                    path.move(to: CGPoint(x: 0, y: 0))
+                    var x = 0
+                    while Float(x) < Float(geometry.size.width) {
+                        x += Int(triangleWidth/2.0)
+                        path.addLine(to: CGPoint(x: x, y: triangleHeight))
+                        x += Int(triangleWidth/2.0)
+                        path.addLine(to: CGPoint(x: x, y: 0))
+                    }
+                }
+                .fill(Color.white)
+            }
+        }
+    }
+}
+
+struct ZigZagSeparatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZigZagBackgroundView(color: .lightGray, numberOfTriangles: 20, triangleHeight: 10)
+    }
+}
