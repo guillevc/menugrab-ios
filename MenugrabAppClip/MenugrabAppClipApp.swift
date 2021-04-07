bf4749f5c01f51367c03558ab207022468f912fc
Guillermo Alfonso Varela Chouciño <guillevch@gmail.com>
2021-04-08 00:31:53 +0200

    build: add app clip

---
+++ MenugrabAppClip/MenugrabAppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenugrabAppClipApp.swift
+//  MenugrabAppClip
+//
+//  Created by Guillermo Alfonso Varela Chouciño on 08/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct MenugrabAppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
